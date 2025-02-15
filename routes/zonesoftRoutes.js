f5bb536d285483bbae472c84d11d4fea86fda43e
contatograciouspt <contatoaledepaula@gmail.com>
2025-02-15 00:15:49 -0300

    intagração com zonesoft status: homologação 85

--- routes/zonesoftRoutes.js
+++ routes/zonesoftRoutes.js
@@ -27,10 +27,10 @@ router.get("/pos/status/", zoneSoftPosOnline)
 router.get("/pos/status/", zoneSoftPosStatus)
 
 // Endpoint para colocar o POS online (DELETE)
-router.delete("/pos/status/closing", zoneSoftPosOnline)
+router.delete("/pos/status/", zoneSoftPosOnline)
 
 // Endpoint para colocar o POS offline (PUT)
-router.put("/pos/status/closing", zoneSoftPosOffline)
+router.put("/pos/status/", zoneSoftPosOffline)
 
 // Endpoint para testar enviar pedido para zoneSoft via POSTMAN
 router.post("/test", zoneSoftOrder)
